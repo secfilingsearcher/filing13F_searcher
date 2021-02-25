92421bfd1b2ca34dc0b690c8f560fa053103372f
Rose <raltianas@gmail.com>
2021-02-24 23:25:15 -0500

    Add files for parser.

---
+++ parser/scraper_company_name.py
@@ -0,0 +1,14 @@
+import requests
+import re
+import unicodedata2
+
+url = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=6&q3=13F'
+
+getter = requests.get(url)
+
+text = getter.text
+
+for filing_url in re.findall('</a>(?!.+</a>)\s*(\w.+)', text, flags=0):
+    filing_url2 = unicodedata2.normalize('NFC', filing_url)
+    print(filing_url2)
+
