d4b0e33782a9b7231b646965169f6614fdd13e82
Rose <raltianas@gmail.com>
2021-03-04 17:04:05 -0500

    updated files

--- parser/parser.py
+++ parser/parser.py
@@ -1,26 +1,37 @@
 import requests
 import re
-# import unicodedata2
 
-url = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=6&q3=13F'
+url_edgar_current_events = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=6&q3=13F'
+getter = requests.get(url_edgar_current_events)
+text_edgar_current_events = getter.text
 
-getter = requests.get(url)
+primary_doc_xml_list = []
+infotable_xml_list = []
 
-text = getter.text
+for partial_url_13f_hr in re.findall('(?<=<a href=")(.*)(?=">13F-HR</a>)', text_edgar_current_events, flags=0):
+    front_sec_url = "https://www.sec.gov"
+    full_url_13f_hr = front_sec_url + partial_url_13f_hr
+    getter = requests.get(full_url_13f_hr)
+    text_13f_hr = getter.text
+    # grab link 1
+    partial_primary_doc_xml_url = re.search('(?<=<a href=")(.*)(?=">primary_doc.html)', text_13f_hr, flags=0)
+    if partial_primary_doc_xml_url:
+        partial_primary_doc_xml_url = partial_primary_doc_xml_url.group(0)
+        full_primary_doc_xml_url = front_sec_url + partial_primary_doc_xml_url
+        primary_doc_xml_list.append(full_primary_doc_xml_url)
+    else:
+        # need to return error
+        print('None')
+    # grab link 2
+    partial_infotable_xml_url = re.search('(?<=<a href=")(.*)(?=">infotable.html)', text_13f_hr, flags=0)
+    if partial_infotable_xml_url:
+        partial_infotable_xml_url = partial_infotable_xml_url.group(0)
+        full_infotable_xml_url = front_sec_url + partial_infotable_xml_url
+        infotable_xml_list.append(full_infotable_xml_url)
+    else:
+        # need to return error
+        print('None')
+    break
 
-company_name = []
-
-# print(re.findall('</a>(?!.+</a>)\s*(\w.+)', text, flags=0))
-
-# company_list = re.search('</strong><hr>.*<hr></pre>', text, re.DOTALL)
-# grouped = company_list.group(0)
-# print(str(grouped))
-#
-
-for company_info in re.findall('(?<=03-03-2021)(.*)', text, flags=0):
-    filing_name = re.search('</a>(?!.+</a>)\s*(\w.+)', company_info)
-    company_url = re.search('(?<=<a href=")(.*)(?=">13F-HR</a>)', company_info)
-    print(company_url)
-    print(filing_name)
-
-
+print(primary_doc_xml_list)
+print(infotable_xml_list)
