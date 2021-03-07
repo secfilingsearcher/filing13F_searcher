836b0468b4509f7032956ed25faeba7d1a4a6221
Edward Harley <edharley19@gmail.com>
2021-03-07 15:35:28 -0500

    Navbar - initial setup

--- frontend/13f-app/src/App.js
+++ frontend/13f-app/src/App.js
@@ -1,11 +1,17 @@
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ frontend/13f-app/src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    return (
+        <>
+         <nav className="navbar">
+             <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    13F <i className="fab fa-typo3" />
+                </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/search' className='nav-links' onClick={closeMobileMenu}>
+                            Search
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                            About
+                        </Link>
+                    </li>
+                </ul>
+             </div>
+         </nav>
+        </>
+        
+    )
+}
+
+export default Navbar
