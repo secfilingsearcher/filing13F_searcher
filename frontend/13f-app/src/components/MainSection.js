fbdde4a6b555b43bb946dfd97a7c7d6ad49df82f
Edward Harley <edharley19@gmail.com>
2021-03-07 18:26:09 -0500

    installed react search box library

---
+++ frontend/13f-app/src/components/MainSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '.App.css';
+import { Button } from Button
+import './MainSection.css';
+
+function MainSection() {
+    return (
+        <div className='main-container'>
+            <h1>COMPANY FINANCIAL HOLDINGS</h1>
+            <p>Search 13 Filings by Company Name and Date</p>
+
+            
+        </div>
+    )
+}
+ 
+ export default MainSection
