9f899e350ccb67d57670d177541c591f3f0f5099
Rose <raltianas@gmail.com>
2021-03-10 00:35:41 -0500

    separated crawler to individual functions that create 3 links of lists

--- parser/crawler_current_events.py
+++ parser/crawler_current_events.py
@@ -2,26 +2,59 @@ import requests
 import re
 
 
-def crawl_page(edgar_13f_filing_detail_url_list, primary_doc_xml_list, infotable_xml_list):
-    url_edgar_current_events = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=6&q3=13F'
-    getter = requests.get(url_edgar_current_events)
-    text_edgar_current_events = getter.text
-
-    for partial_url_13f_hr in re.findall('(?<=<a href=")(.*)(?=">13F)', text_edgar_current_events, flags=0):
-        front_sec_url = "https://www.sec.gov"
-        full_url_13f_hr = front_sec_url + partial_url_13f_hr
-        getter = requests.get(full_url_13f_hr)
-        text_13f_hr = getter.text
-        # get filing detail_page
-        edgar_13f_filing_detail_url_list.append(full_url_13f_hr)
-        # grab link 1 + 2
-        partial_infotable_xml_url = re.findall('(?<=<a href=")(.*)(?=">.*.xml)', text_13f_hr, flags=0)
-        if partial_infotable_xml_url:
-            full_primary_doc_xml_url = front_sec_url + partial_infotable_xml_url[0]
-            full_infotable_xml_url = front_sec_url + partial_infotable_xml_url[-1]
-            primary_doc_xml_list.append(full_primary_doc_xml_url)
-            infotable_xml_list.append(full_infotable_xml_url)
-        else:
-            raise TypeError("Can't find URL on current webpage")
+def grab_text(url):
+    getter = requests.get(url)
+    full_text = getter.text
+    return full_text
 
 
+def get_13f_filing_detail_urls_in_list(edgar_current_events_text, edgar_13f_filing_detail_url_list):
+    prefix_sec_url = "https://www.sec.gov"
+    for suffix_13f_filing_detail_url in re.findall('(?<=<a href=")(.*)(?=">13F)', edgar_current_events_text, flags=0):
+        full_13f_filing_detail_url = prefix_sec_url + suffix_13f_filing_detail_url
+        edgar_13f_filing_detail_url_list.append(full_13f_filing_detail_url)
+    return edgar_13f_filing_detail_url_list
+
+def get_primary_doc_and_infotable_urls(edgar_13f_filing_detail_text):
+    suffix_all_xml_urls = re.findall('(?<=<a href=")(.*)(?=">.*.xml)', edgar_13f_filing_detail_text, flags=0)
+    return suffix_all_xml_urls
+
+
+def get_primary_doc_xml_urls_in_list(suffix_all_xml_urls, primary_doc_xml_list):
+    prefix_sec_url = "https://www.sec.gov"
+    if suffix_all_xml_urls:
+        full_primary_doc_xml_url = prefix_sec_url + suffix_all_xml_urls[0]
+        primary_doc_xml_list.append(full_primary_doc_xml_url)
+    else:
+        raise TypeError("Can't find URL on current webpage")
+
+
+def get_infotable_xml_urls_in_list(partial_xml_url, infotable_xml_list):
+    prefix_sec_url = "https://www.sec.gov"
+    if partial_xml_url:
+        full_infotable_xml_url = prefix_sec_url + partial_xml_url[-1]
+        infotable_xml_list.append(full_infotable_xml_url)
+    else:
+        raise TypeError("Can't find URL on current webpage")
+
+# def crawl_page_old(edgar_13f_filing_detail_url_list, primary_doc_xml_list, infotable_xml_list):
+#     url_edgar_current_events = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=6&q3=13F'
+#     getter = requests.get(url_edgar_current_events)
+#     edgar_current_events_text = getter.text
+#
+#     for partial_url_13f_hr in re.findall('(?<=<a href=")(.*)(?=">13F)', edgar_current_events_text, flags=0):
+#         prefix_sec_url = "https://www.sec.gov"
+#         full_url_13f_hr = prefix_sec_url + partial_url_13f_hr
+#         getter = requests.get(full_url_13f_hr)
+#         text_13f_hr = getter.text
+#         # get filing detail_page
+#         edgar_13f_filing_detail_url_list.append(full_url_13f_hr)
+#         # grab link 1 + 2
+#         partial_xml_url = re.findall('(?<=<a href=")(.*)(?=">.*.xml)', text_13f_hr, flags=0)
+#         if partial_xml_url:
+#             full_primary_doc_xml_url = prefix_sec_url + partial_xml_url[0]
+#             full_infotable_xml_url = prefix_sec_url + partial_xml_url[-1]
+#             primary_doc_xml_list.append(full_primary_doc_xml_url)
+#             infotable_xml_list.append(full_infotable_xml_url)
+#         else:
+#             raise TypeError("Can't find URL on current webpage")
--- parser/main.py
+++ parser/main.py
@@ -1,5 +1,9 @@
 import pandas as pd
-from crawler_current_events import crawl_page
+from crawler_current_events import grab_text
+from crawler_current_events import get_13f_filing_detail_urls_in_list
+from crawler_current_events import get_primary_doc_and_infotable_urls
+from crawler_current_events import get_primary_doc_xml_urls_in_list
+from crawler_current_events import get_infotable_xml_urls_in_list
 from parser_13f_primary_doc import primary_doc_parser
 from parser_13f_infotable_V2 import infotable_parser
 
@@ -9,20 +13,30 @@ infotable_list = []
 
 # TODO; make function with no arg that returns lists that can be used outside
 
+
 def main():
-    crawl_page(edgar_13f_list, primary_doc_list, infotable_list)
-    # create for loop and use one row function to get cik
-    for primary_doc in primary_doc_list:
-        primary_doc_parser(primary_doc)
-    # create for loop and use infotable function to get database
-    df = pd.DataFrame()
-    for infotable in infotable_list:
-        infotable_parser(infotable, df)
+    url_edgar_current_events = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=6&q3=13F'
+    text_edgar_current_events = grab_text(url_edgar_current_events)
+    all_urls_13f = get_13f_filing_detail_urls_in_list(text_edgar_current_events, edgar_13f_list)
+
+    for urls_13f in all_urls_13f:
+        filing_detail_13f_text = grab_text(urls_13f)
+        xml_links = get_primary_doc_and_infotable_urls(filing_detail_13f_text)
+        get_primary_doc_xml_urls_in_list(xml_links, primary_doc_list)
+        get_infotable_xml_urls_in_list(xml_links, infotable_list)
+
+    # # create for loop and use one row function to get cik
+    # for primary_doc in primary_doc_list:
+    #     primary_doc_parser(primary_doc)
+    # # create for loop and use infotable function to get database
+    # df = pd.DataFrame()
+    # for infotable in infotable_list:
+    #     infotable_parser(infotable, df)
 
 if __name__ == "__main__":
     main()
 
-# print("check")
-# print(edgar_13f_list)
-# print(primary_doc_list)
-# print(infotable_list)
+print("check")
+print(edgar_13f_list)
+print(primary_doc_list)
+print(infotable_list)
