65ea855cd7901865a0ffe2e167e83d87db4d85c6
Rose <raltianas@gmail.com>
2021-03-10 22:59:15 -0500

    Refactor file names

--- parser/main.py
+++ parser/main.py
@@ -1,12 +1,12 @@
-from crawler_edgar_current_events import grab_text
-from crawler_edgar_current_events import get_13f_filing_detail_urls
-from crawler_edgar_current_events import get_primary_doc_and_infotable_urls
-from crawler_edgar_current_events import get_primary_doc_xml_url
-from crawler_edgar_current_events import get_infotable_xml_url
-from parser_13f_primary_doc_xml import grab_primary_doc_root
-from parser_13f_primary_doc_xml import grab_primary_doc_cik
-from parser_13f_primary_doc_xml import grab_primary_doc_company_name
-from parser_13f_infotable_xml import grab_infotable
+from crawler_current_events import grab_text
+from crawler_current_events import get_13f_filing_detail_urls
+from crawler_current_events import get_primary_doc_and_infotable_urls
+from crawler_current_events import get_primary_doc_xml_url
+from crawler_current_events import get_infotable_xml_url
+from primary_doc_xml import grab_primary_doc_root
+from primary_doc_xml import grab_primary_doc_cik
+from primary_doc_xml import grab_primary_doc_company_name
+from infotable_xml import grab_infotable
 
 
 def main():
