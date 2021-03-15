ed2cdd0a10b70fe9534cdc91f981acc8269e0b94
Rose <raltianas@gmail.com>
2021-03-15 15:14:30 -0400

    Insert accepted_filing_date into infotable table

--- parser/main.py
+++ parser/main.py
@@ -28,10 +28,11 @@ def main():
         cik = grab_primary_doc_cik(root)
         company_name = grab_primary_doc_company_name(root)
         accepted_filing_date = grab_primary_doc_accepted_filing_date(root)
-        print(cik, company_name, accepted_filing_date)
+        print(cik, company_name)
 
         df_infotable = grab_infotable(infotable_xml_url)
         df_infotable.insert(loc=0, column='cik_id', value=cik)
+        df_infotable.insert(loc=2, column='accepted_filing_date', value=accepted_filing_date)
         print(df_infotable.head())
 
 
