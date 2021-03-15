eb22faeb1755dde4f2e620d09d84861f22f422f8
Rose <raltianas@gmail.com>
2021-03-15 14:56:35 -0400

    Reformat filing date on main

--- parser/main.py
+++ parser/main.py
@@ -11,6 +11,7 @@ from primary_doc_xml import grab_primary_doc_accepted_filing_date
 from infotable_xml import grab_infotable
 from database_connection import *
 
+
 def main():
     """function docstring"""
     url_edgar_current_events = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=6&q3=13F'
@@ -18,7 +19,6 @@ def main():
     filing_detail_urls = get_13f_filing_detail_urls(text_edgar_current_events)
 
     for filing_detail_url in filing_detail_urls:
-
         filing_detail_text = grab_text(filing_detail_url)
         xml_links = get_primary_doc_and_infotable_urls(filing_detail_text)
         primary_doc_xml_url = get_primary_doc_xml_url(xml_links)
@@ -30,12 +30,9 @@ def main():
         accepted_filing_date = grab_primary_doc_accepted_filing_date(root)
         print(cik, company_name, accepted_filing_date)
 
-
         df_infotable = grab_infotable(infotable_xml_url)
         df_infotable.insert(loc=0, column='cik_id', value=cik)
         print(df_infotable.head())
-
-
 
 
 if __name__ == "__main__":
