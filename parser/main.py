80704720e9919fa887c299cacb597a62ae3c3d46
rosealti <71480280+rosealti@users.noreply.github.com>
2021-03-18 00:28:33 -0400

    Feature/add unique accession no 13f (#12)
    Add sec_accession_no to pandas dataframe, merge main, fixed sec_accession_no function bug on main.py, reorder imported files on main.py, and add docstring to sec_accession_no .

--- parser/main.py
+++ parser/main.py
@@ -1,14 +1,15 @@
 """This file returns the cik, company name, and infotable data"""
 from crawler_current_events import get_text
 from crawler_current_events import get_13f_filing_detail_urls
+from crawler_current_events import extract_sec_accession_no
 from crawler_current_events import get_primary_doc_and_infotable_urls
 from crawler_current_events import get_primary_doc_xml_url
 from crawler_current_events import get_infotable_xml_url
+from infotable_xml import get_infotable
 from primary_doc_xml import get_primary_doc_root
 from primary_doc_xml import get_primary_doc_cik
 from primary_doc_xml import get_primary_doc_company_name
 from primary_doc_xml import get_primary_doc_accepted_filing_date
-from infotable_xml import get_infotable
 
 
 def main():
@@ -19,6 +20,7 @@ def main():
 
     for filing_detail_url in filing_detail_urls:
         filing_detail_text = get_text(filing_detail_url)
+        sec_accession_no = extract_sec_accession_no(filing_detail_text)
         xml_links = get_primary_doc_and_infotable_urls(filing_detail_text)
         primary_doc_xml_url = get_primary_doc_xml_url(xml_links)
         infotable_xml_url = get_infotable_xml_url(xml_links)
@@ -27,10 +29,11 @@ def main():
         cik = get_primary_doc_cik(root)
         company_name = get_primary_doc_company_name(root)
         filing_date = get_primary_doc_accepted_filing_date(root)
-        print(cik, company_name)
+        print(cik, company_name, filing_date)
 
         df_infotable = get_infotable(infotable_xml_url)
         df_infotable.insert(loc=0, column='cik_id', value=cik)
+        df_infotable.insert(loc=1, column='accession_no', value=sec_accession_no)
         df_infotable.insert(loc=2, column='filing_date', value=filing_date)
         print(df_infotable.head())
 
