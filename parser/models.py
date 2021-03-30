23034d6ac3e811b5d918555d96f1e8430ba6b7d4
Rose <raltianas@gmail.com>
2021-03-30 18:34:54 -0400

    Reformat models.py

--- parser/models.py
+++ parser/models.py
@@ -5,6 +5,19 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
+class PrimaryDoc(Base):
+    """Define PrimaryDoc Table"""
+    __tablename__ = 'primary_doc'
+    id = Column(String(50), primary_key=True)
+    cik = Column(String(50))
+    company_name = Column(String(50))
+    filing_date = Column(Date)
+
+    def __repr__(self):
+        return "<User(cik='%s', filing_date='%s', company_name='%s')>" % (
+            self.cik, self.filing_date, self.company_name)
+
+
 class Infotable(Base):
     """Define Infotable Table"""
     __tablename__ = 'infotable'
@@ -35,16 +48,3 @@ class Infotable(Base):
                    self.sshPrnamt, self.sshPrnamtType, self.putCall,
                    self.investmentDiscretion, self.otherManager, self.votingAuthority_Sole,
                    self.votingAuthority_Shared, self.votingAuthority_None)
-
-
-class PrimaryDoc(Base):
-    """Define PrimaryDoc Table"""
-    __tablename__ = 'primary_doc'
-    id = Column(String(50), primary_key=True)
-    cik = Column(String(50))
-    company_name = Column(String(50))
-    filing_date = Column(Date)
-
-    def __repr__(self):
-        return "<User(cik='%s', filing_date='%s', company_name='%s')>" % (
-            self.cik, self.filing_date, self.company_name)
