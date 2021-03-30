0710d1a0badf8d44a51b7b5cb8439b335cff2710
rosebertaltianas <raltianas@gmail.com>
2021-03-30 01:32:08 -0400

    Import typing for list

--- parser/database_connection.py
+++ parser/database_connection.py
@@ -1,7 +1,8 @@
 """Create database connection"""
 import os
+from typing import List
+
 from sqlalchemy.orm import sessionmaker
-import pandas as pd
 from models import PrimaryDoc
 from orm import get_engine
 
@@ -9,13 +10,14 @@ engine = get_engine(os.environ.get('DB_CONNECTION_STRING'), echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
 def insert_in_database_primary_table(id, cik, company_name, filing_date):
     """Insert list in primary_doc table"""
-    session.merge(PrimaryDoc(id=id, cik=cik, company_name=company_name, filing_date=filing_date))
+    session.add(PrimaryDoc(id=id, cik=cik, company_name=company_name, filing_date=filing_date))
     session.commit()
 
 
-def insert_in_database_infotable_table(infotable_table : list):
+def insert_in_database_infotable_table(infotable_table: List):
     """Insert dataframe in infotable table"""
     session.add_all(infotable_table)
     session.commit()
