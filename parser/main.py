90a9b3b3a47878ac2b0044ea18e5cc624df72c1f
Rose <raltianas@gmail.com>
2021-03-30 22:44:15 -0400

    Update url_edgar_current_events link

--- parser/main.py
+++ parser/main.py
@@ -9,7 +9,7 @@ from primary_doc_xml import get_primary_doc_root, get_primary_doc_cik, \
 
 def main():
     """This function returns the cik_value, company name, and infotable data"""
-    url_edgar_current_events = 'https://www.sec.gov/cgi-bin/current?q1=1&q2=0&q3=13f'
+    url_edgar_current_events = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=6&q3=13F'
     text_edgar_current_events = get_text(url_edgar_current_events)
     filing_detail_urls = get_13f_filing_detail_urls(text_edgar_current_events)
 
