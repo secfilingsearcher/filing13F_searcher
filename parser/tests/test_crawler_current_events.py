65bef02453cc33b38a490cde85407ae8984cff3a
rosebertaltianas <raltianas@gmail.com>
2021-04-07 21:23:43 -0400

    Add new html file to commit

--- parser/tests/test_crawler_current_events.py
+++ parser/tests/test_crawler_current_events.py
@@ -11,9 +11,19 @@ def current_events_text():
     return myfile.read()
 
 
+@pytest.fixture
+def filing_detail_text():
+    myfile = open("fixtures/edgar_current_events.html", "rt")
+    return myfile.read()
+
+
 def test_get_13f_filing_detail_urls(current_events_text):
     assert get_13f_filing_detail_urls(current_events_text) == [
         'https://www.sec.gov/Archives/edgar/data/1850858/0001850858-21-000001-index.html',
         'https://www.sec.gov/Archives/edgar/data/1852858/0001852858-21-000001-index.html',
         'https://www.sec.gov/Archives/edgar/data/1835714/0001085146-21-001095-index.html',
         'https://www.sec.gov/Archives/edgar/data/1446194/0001011712-21-000002-index.html']
+
+
+def get_sec_accession_no(filing_detail_text):
+    pass
