62df06e5fa7101a7a1f8557c1bc2d7813f5c7381
ralti <raltianas@gmail.com>
2021-04-26 00:59:56 -0400

    Change function names

--- parser/main.py
+++ parser/main.py
@@ -1,7 +1,7 @@
 # pylint: disable=import-error
 """This file contains the main method"""
 from crawler_current_events import get_text, parse_13f_filing_detail_urls, parse_sec_accession_no, \
-    parse_primary_doc_and_infotable_urls, parse_primary_doc_xml_url, parse_infotable_xml_url
+    parse_primary_doc_xml_and_infotable_xml_urls, parse_primary_doc_xml_url, parse_infotable_xml_url
 from database_connection import session
 from data_13f import data_13f_row
 from filing_13f import parse_primary_doc_root, parse_primary_doc_cik, \
@@ -23,7 +23,7 @@ def main():
     for filing_detail_url in filing_detail_urls:
         filing_detail_text = get_text(filing_detail_url)
         accession_no = parse_sec_accession_no(filing_detail_text)
-        xml_links = parse_primary_doc_and_infotable_urls(filing_detail_text)
+        xml_links = parse_primary_doc_xml_and_infotable_xml_urls(filing_detail_text)
         primary_doc_xml_url = parse_primary_doc_xml_url(xml_links)
         infotable_xml_url = parse_infotable_xml_url(xml_links)
 
