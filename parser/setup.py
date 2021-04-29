67ae90eaf1a97d8b834a57c8f1ddc0bcb4bae310
Rose <raltianas@gmail.com>
2021-04-28 21:15:19 -0400

    Add setup.py

---
+++ parser/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='filing13F_searcher',
+    version='0.0.1',
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ]
+)
