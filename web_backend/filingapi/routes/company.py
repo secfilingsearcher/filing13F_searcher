61121f2092a978b266947c84d9c9f0ef31138c7b
Edward Harley <edharley19@gmail.com>
2021-05-02 19:43:54 -0400

    Remove commented-out code

--- web_backend/filingapi/routes/company.py
+++ web_backend/filingapi/routes/company.py
@@ -10,17 +10,3 @@ def get_company(company_id):
     """Route for results for search by company id"""
     companies = Company.query.filter(Company.cik_no.like(f"{company_id}%"))
     return jsonify(list(companies))
-
-#
-# @company_blueprint.route('/company/<company_id>/filings')
-# def get_filings(company_id):
-#     """Route for results for retrieval of filings"""
-#     filings = Infotable.query.filter(Infotable.cik_no == company_id)
-#     return jsonify(list(filings))
-#
-#
-# @company_blueprint.route('/filings/<filing_id>')
-# def get_filing(filing_id):
-#     """Route for result of search for specific filing by filing_id"""
-#     filing = Infotable.query.filter(Infotable.row_id == filing_id).first()
-#     return jsonify(filing)
