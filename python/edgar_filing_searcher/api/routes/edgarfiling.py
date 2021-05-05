17e1bed5d301f9813c0d5df419a0172d4a5e46b8
Edward Harley <edharley19@gmail.com>
2021-05-05 00:22:52 -0400

    Add date to search parameters

--- python/edgar_filing_searcher/api/routes/edgarfiling.py
+++ python/edgar_filing_searcher/api/routes/edgarfiling.py
@@ -1,6 +1,7 @@
 """APi for web back-end"""
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from edgar_filing_searcher.models import EdgarFiling
+from datetime import datetime
 
 filing_blueprint = Blueprint('edgarfiling', __name__)
 
@@ -8,5 +9,11 @@ filing_blueprint = Blueprint('edgarfiling', __name__)
 @filing_blueprint.route('/edgarfiling/<company_id>')
 def get_filings(company_id):
     """Route for results for search by company id"""
-    filings = EdgarFiling.query.filter(EdgarFiling.cik_no.like(f"{company_id}%"))
+    date_format = '%Y-%m-%d'
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    filings = EdgarFiling.query.filter(EdgarFiling.cik_no.like(f"{company_id}%"),
+                                       EdgarFiling.filing_date >= datetime.strptime(start_date, date_format),
+                                       EdgarFiling.filing_date <= datetime.strptime(end_date, date_format))
+
     return jsonify(list(filings))
