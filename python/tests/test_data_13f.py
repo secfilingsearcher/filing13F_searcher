d80604b57042d2c5419788a91eea21a05f191ef6
Rose <raltianas@gmail.com>
2021-05-06 00:18:00 -0400

    Add test_data_13f.py

--- python/tests/test_data_13f.py
+++ python/tests/test_data_13f.py
@@ -4,3 +4,15 @@ from xml.etree import ElementTree
 from edgar_filing_searcher.parsers.crawler_current_events import get_text
 from edgar_filing_searcher.models import Data13f
 import pytest
+
+
+def test_data_13f_row():
+    assert False
+
+
+def test_parse_infotable_doc_root():
+    assert False
+
+
+def test_parse_xml_text():
+    assert False
