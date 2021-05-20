493d78f0fb93e58b726cc558824350a68e82aee2
Edward Harley <edharley19@gmail.com>
2021-05-20 00:46:06 -0400

    Delete button component

--- frontend/13f-app/src/components/SearchForm.js
+++ frontend/13f-app/src/components/SearchForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../App.css'
-import { Button } from './Button'
+import './Button.css'
 import './MainSection.css'
 
 const divStyle = { float: 'left', padding: '20px', margin: '20px' }
@@ -9,15 +10,21 @@ const today = date.toISOString().substr(0, 10)
 
 function SearchForm () {
   const [searchName, setSearchName] = useState('')
-  const handleClick = event => { setSearchName(event.target.value) }
+  const [searchStartDate, setSearchStartDate] = useState(today)
+  const [searchEndDate, setSearchEndDate] = useState(today)
+  const handleNameChange = event => { setSearchName(event.target.value) }
+  const handleStartDateChange = event => { setSearchStartDate(event.target.value) }
+  const handleEndDateChange = event => { setSearchEndDate(event.target.value) }
+  const handleClick = (e) => console.log(searchName)
+
   return (
         <>
-              <div style={divStyle}>
-                <input type='text' placeholder='Company Name' className='search' value={searchName}></input>
-                <input type='date' className='date' defaultValue={today}></input>
-                <input type='date' className='date' defaultValue={today}></input>
-              </div>
-              <Button buttonStyle='btn--outline' buttonSize='btn--large' onClick={handleClick}>SEARCH</Button>
+                <div style={divStyle}>
+                  <input type='text' placeholder='Company Name' className='search' value={searchName} onChange={handleNameChange}></input>
+                  <input type='date' className='date' defaultValue={today} value={searchStartDate} onChange={handleStartDateChange}></input>
+                  <input type='date' className='date' defaultValue={today} value={searchEndDate} onChange={handleEndDateChange}></input>
+                </div>
+                <Link to="/search"><button className="button" onClick={handleClick} type="submit">SEARCH</button></Link>
         </>
   )
 }
