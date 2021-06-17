441ddb7f2d12859170fb913ad49236d1e506ce4a
Rosebert Altianas <raltianas@gmail.com>
2021-06-16 23:43:29 -0400

    Add comment to test_main.py

---
+++ python/tests/parsers/test_main.py
@@ -0,0 +1,15 @@
+# pylint: disable=redefined-outer-name
+"""This file contains tests for main"""
+def test_create_url_list():
+    """This function tests create_url_list"""
+    assert False
+
+
+def test_send_data_to_db():
+    """This function tests send_data_to_db"""
+    assert False
+
+
+def test_main():
+    """This function tests main"""
+    assert False
