a00bbf0773b765414a756bc7c6a08758f8023329
eharley19 <48498743+eharley19@users.noreply.github.com>
2021-06-16 23:49:32 -0400

    Add footer component (#61)

--- frontend/13f-app/src/App.js
+++ frontend/13f-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/pages/home'
 import SearchResults from './components/pages/searchResults'
@@ -14,6 +15,7 @@ function App () {
         <Route path='/' exact component= {Home} />
         <Route path='/search' exact component= {SearchResults} />
       </Switch>
+      <Footer />
     </Router>
     </>
   )
