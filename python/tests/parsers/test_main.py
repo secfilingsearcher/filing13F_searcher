cca943f0290f80389d3ae027aeed5d3a05de4ecd
rosebertaltianas <raltianas@gmail.com>
2021-06-21 02:49:04 -0400

    Set up Flask_Testing

--- python/tests/parsers/test_main.py
+++ python/tests/parsers/test_main.py
@@ -1,3 +1,6 @@
+from flask import Flask
+from flask_testing import TestCase
+
 # pylint: disable=redefined-outer-name
 """This file contains tests for main"""
 def test_create_url_list():
@@ -10,6 +13,11 @@ def test_send_data_to_db():
     assert False
 
 
-def test_main():
+def test_main(TestCase):
     """This function tests main"""
+    def create_app(self):
+
+    app = Flask(__name__)
+    app.config['TESTING'] = True
+    return app
     assert False
