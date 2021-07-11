8df3e7a9efc1dd07c50c09fd6bb38d7313c5395e
RA <raltianas@gmail.com>
2021-07-11 19:11:38 -0400

    Rename paramenter

--- python/edgar_filing_searcher/api/__init__.py
+++ python/edgar_filing_searcher/api/__init__.py
@@ -5,13 +5,13 @@ from edgar_filing_searcher.database import db
 from edgar_filing_searcher.api.routes.company import company_blueprint
 
 
-def create_app(obj=None):
+def create_app(configuration_file_obj=None):
     """Create Flask app and create database."""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    if obj:
-        app.config.from_object(obj)
+    if configuration_file_obj:
+        app.config.from_object(configuration_file_obj)
     db.init_app(app)
     app.register_blueprint(company_blueprint)
     return app
