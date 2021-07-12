f86d54574cf65eabf8340813c43517f1b857f2bc
RA <raltianas@gmail.com>
2021-07-12 00:04:39 -0400

    Update main.py and crawler_current_events.py logging

--- python/edgar_filing_searcher/parsers/main.py
+++ python/edgar_filing_searcher/parsers/main.py
@@ -15,37 +15,37 @@ URL_EDGAR_CURRENT_EVENTS = 'https://www.sec.gov/cgi-bin/current?q1=0&q2=0&q3=13f
 
 def create_url_list(url_edgar_current_events):
     """This function creates a list of URLs"""
-    logging.info('Extracting URL %s', url_edgar_current_events)
     text_edgar_current_events = get_text(url_edgar_current_events)
+    logging.info('Extracted URL %s', url_edgar_current_events)
     return parse_13f_filing_detail_urls(text_edgar_current_events)
 
 
 def update_filing_counts(cik_no_list):
     """This function counts the number of filings and adds it to the Company table"""
-    logging.info('Add number of filings for Company table')
     for cik_no in cik_no_list:
         company_in_table = Company.query.filter_by(cik_no=cik_no).first()
         filing_count = EdgarFiling.query.filter_by(cik_no=cik_no).count()
         company_in_table.filing_count = filing_count
-        logging.debug('Count amount of filings for cik no %s. Count: %i', cik_no, filing_count)
+        logging.debug('Counted amount of filings for CIK: %s. Count: %i', cik_no, filing_count)
         db.session.commit()
+        logging.info('Added number of filings for CIK: %s to Company table', cik_no)
 
 
 def send_data_to_db(company_row, edgar_filing_row, data_13f_table):
     """This function sends data to the database"""
-    logging.info('Send data to Database')
-    logging.debug('Start sending company_row, edgar_filing_row, data_13f_table')
     db.session.merge(company_row)
     db.session.merge(edgar_filing_row)
     for data_13f_row in data_13f_table:
         db.session.merge(data_13f_row)
         logging.debug('Data_13f_row session merged %s', data_13f_row)
     db.session.commit()
+    logging.info('Sent company_row, edgar_filing_row, data_13f_table data to Database')
 
 
 def main():
     """This function returns the cik, company name, and infotable data"""
-    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+    logging.getLogger('sqlalchemy').setLevel(logging.WARNING)
     logging.info('Initializing job')
     filing_detail_urls = create_url_list(URL_EDGAR_CURRENT_EVENTS)
     if not filing_detail_urls:
--- python/edgar_filing_searcher/parsers/parser_context.py
+++ python/edgar_filing_searcher/parsers/parser_context.py
@@ -10,7 +10,7 @@ def create_app():
     """This function creates a Flask SQLAlchemy app context to be used for sqlalchemy parser"""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
-    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_ECHO'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
