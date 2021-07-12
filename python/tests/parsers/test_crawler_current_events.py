bbe3c4e921e412ff93527e16bef3ef1aff112a1f
RA <raltianas@gmail.com>
2021-07-11 21:40:19 -0400

    Accepted and addressed suggested changes.

--- python/tests/parsers/test_crawler_current_events.py
+++ python/tests/parsers/test_crawler_current_events.py
@@ -32,11 +32,11 @@ def test_parse_13f_filing_detail_urls(current_events_text):
 
     actual = parse_13f_filing_detail_urls(current_events_text)
 
-    assert actual == ['https://www.sec.gov/Archives/edgar/data/1850858/0001850858-21-000001-index.html',
-                      'https://www.sec.gov/Archives/edgar/data/1852858/0001852858-21-000001-index.html',
-                      'https://www.sec.gov/Archives/edgar/data/1835714/0001085146-21-001095-index.html',
-                      'https://www.sec.gov/Archives/edgar/data/1446194/0001011712-21-000002-index.html'
-                      ]
+    assert actual == \
+           ['https://www.sec.gov/Archives/edgar/data/1850858/0001850858-21-000001-index.html',
+            'https://www.sec.gov/Archives/edgar/data/1852858/0001852858-21-000001-index.html',
+            'https://www.sec.gov/Archives/edgar/data/1835714/0001085146-21-001095-index.html',
+            'https://www.sec.gov/Archives/edgar/data/1446194/0001011712-21-000002-index.html']
 
 
 def test_parse_sec_accession_no(filing_detail_text):
@@ -52,9 +52,9 @@ def test_parse_primary_doc_xml_and_infotable_xml_urls(filing_detail_text):
 
     actual = parse_primary_doc_xml_and_infotable_xml_urls(filing_detail_text)
 
-    assert actual == ['/Archives/edgar/data/1852858/000185285821000001/primary_doc.xml',
-                      '/Archives/edgar/data/1852858/000185285821000001/infotable.xml'
-                      ]
+    assert actual == \
+           ['/Archives/edgar/data/1852858/000185285821000001/primary_doc.xml',
+            '/Archives/edgar/data/1852858/000185285821000001/infotable.xml']
 
 
 def test_parse_primary_doc_xml_url(xml_list):
