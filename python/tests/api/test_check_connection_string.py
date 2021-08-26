e3b396c7fcc4274f2aa3581d5bdff77415db1b5c
RA <raltianas@gmail.com>
2021-08-25 23:17:04 -0400

    Add test

--- python/tests/api/test_check_connection_string.py
+++ python/tests/api/test_check_connection_string.py
@@ -9,12 +9,16 @@ from edgar_filing_searcher.api.routes.check_connection_string import postgres_te
 from edgar_filing_searcher.parsers.errors import InvalidConnectionStringException
 
 
+def check_connection_string():
+    pass
+
+
 def test_check_connection_string():
     """Tests if parse_13f_filing_detail_urls raises the NoUrlException exception"""
-    os.environ["DB_CONNECTION_STRING"] = ""
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
-    with pytest.raises(NoUrlException):
-        parse_13f_filing_detail_urls("")
+    os.environ["DB_CONNECTION_STRING"] = "sqlite://"
+    actual = check_connection_string()
+    assert actual == \
+           ['https://www.sec.gov/Archives/edgar/data/1850858/0001850858-21-000001-index.html']
 
 
 def test_check_connection_string_invalidConnectionString_raiseException():
