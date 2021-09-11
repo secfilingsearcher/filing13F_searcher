a99fe67413550c78f3825b24cd932815c5a3a4f4
RA <raltianas@gmail.com>
2021-09-11 17:07:26 -0400

    Update docstrings and update endpoints.

--- python/edgar_filing_searcher/api/routes/company.py
+++ python/edgar_filing_searcher/api/routes/company.py
@@ -18,7 +18,7 @@ def after_request(response):
 
 @company_blueprint.route('/company/search')
 def get_company():
-    """Route for results for search by company name"""
+    """Route for search results by company name"""
     company_name = request.args.get('q')
     if company_name:
         companies = Company.query.filter(Company.company_name.ilike(f"%{company_name}%"))
@@ -29,7 +29,7 @@ def get_company():
 
 @company_blueprint.route('/company/<company_id>/edgarfiling/')
 def get_filings(company_id):
-    """Route for results for search by company id and date"""
+    """Route for search results of filings by company id and date"""
     date_format = '%Y-%m-%d'
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
@@ -49,14 +49,14 @@ def get_filings(company_id):
 
 @company_blueprint.route('/company/<company_id>/')
 def get_filings_from_company(company_id):
-    """Route for results for search by company id"""
-    filings = EdgarFiling.query.filter(EdgarFiling.cik_no.like(f"{company_id}%"))
+    """Route for search results of company by company id"""
+    filings = EdgarFiling.query.filter(EdgarFiling.cik_no == company_id)
     return jsonify(list(filings))
 
 
-@company_blueprint.route('/company/<company_id>/<accession_id>')
-def get_filings_from_company_(company_id, accession_id):
-    """Route for results for search by accession no"""
+@company_blueprint.route('/company/<company_id>/filing/<filing_id>')
+def get_filings_from_company_(company_id, filing_id):
+    """Route for search results of filing by filing id"""
     data13f = Data13f.query. \
-        filter(Data13f.cik_no == company_id, Data13f.accession_no.like(f"{accession_id}%"))
+        filter(Data13f.cik_no == company_id, Data13f.accession_no == filing_id)
     return jsonify(list(data13f))
