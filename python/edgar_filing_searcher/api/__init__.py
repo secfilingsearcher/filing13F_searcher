d91ac3461be6b087dd8c04f7c881d4394e018136
rosealti <71480280+rosealti@users.noreply.github.com>
2021-09-15 04:04:55 -0400

    Update __init__.py

--- python/edgar_filing_searcher/api/__init__.py
+++ python/edgar_filing_searcher/api/__init__.py
@@ -3,7 +3,6 @@ import os
 
 from flask import Flask
 
-from edgar_filing_searcher.api.routes.check_connection_string import postgres_test
 from edgar_filing_searcher.api.routes.company import company_blueprint
 from edgar_filing_searcher.database import db
 
@@ -11,7 +10,6 @@ from edgar_filing_searcher.database import db
 def create_app(configuration_file_obj=None):
     """Create Flask app and create database."""
     app = Flask(__name__)
-    postgres_test()
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     if configuration_file_obj:
