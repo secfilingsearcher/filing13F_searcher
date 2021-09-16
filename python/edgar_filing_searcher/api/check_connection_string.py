62fb3e7936d9bc8bd67ba70f23149eda5ebd49cd
RA <raltianas@gmail.com>
2021-09-16 00:02:48 -0400

    Moved files

--- python/edgar_filing_searcher/api/check_connection_string.py
+++ python/edgar_filing_searcher/api/check_connection_string.py
@@ -1,5 +1,4 @@
 """This file checks the connection string"""
-import logging
 import psycopg2
 
 from edgar_filing_searcher.parsers.errors import InvalidConnectionStringException
@@ -10,7 +9,5 @@ def check_postgres_connection_string(db_connection_string: str):
     try:
         conn = psycopg2.connect(db_connection_string)
         conn.close()
-        logging.info('%s Connection Successful', conn)
     except Exception as error:
-        logging.critical("Invalid Url")
-        raise InvalidConnectionStringException("Invalid Database Connection String Url") from error
+        raise InvalidConnectionStringException(error, "Invalid Database Connection String Url")
