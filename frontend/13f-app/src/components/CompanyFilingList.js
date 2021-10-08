107aeda1f3d3a1098f9f27ae0457bc814f9faef5
Ed <edharley19@gmail.com>
2021-10-08 02:24:02 -0400

    Add css styling for company filing list

--- frontend/13f-app/src/components/CompanyFilingList.js
+++ frontend/13f-app/src/components/CompanyFilingList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useLocation, useParams, Link } from 'react-router-dom'
 import Table from 'react-bootstrap/Table'
 import axios from 'axios'
-import './CompanyFilingList.css'
+import './resultstable.css'
 
 function FilingsList () {
   const [results, setResults] = useState([])
@@ -16,10 +16,9 @@ function FilingsList () {
       })
   }, [])
   return (
-        <div id='filings-list'>
-            <h1>{state.company_name}</h1>
-            <div id="table-container">
-              <Table borderless hover>
+        <div id='table-container'>
+            <h2>{state.company_name}</h2>
+              <Table borderless>
                 <thead>
                   <tr>
                       <th>Accession Number</th>
@@ -29,14 +28,13 @@ function FilingsList () {
                 <tbody>
                   {results.map(result => (
                       <tr key={result.accession_no}>
-                        <td><Link to={{ pathname: `/company/${companyId}/edgar-filing/${result.accession_no}/data/` }} className="link-style">{result.accession_no}</Link></td>
+                        <td className="accession-no"><Link to={{ pathname: `/company/${companyId}/edgar-filing/${result.accession_no}/data/` }} className="link-style">{result.accession_no}</Link></td>
                         <td className="filing-date">{result.filing_date}</td>
                       </tr>
                   ))}
                 </tbody>
               </Table>
-            </div>
-          </div>
+        </div>
   )
 }
 
