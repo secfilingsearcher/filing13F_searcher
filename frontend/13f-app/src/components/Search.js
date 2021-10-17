2e1d38341799720e193605dc82f11cb828e16048
Ed <edharley19@gmail.com>
2021-10-17 11:03:30 -0400

    Add new search component using Formik library

---
+++ frontend/13f-app/src/components/Search.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import { useHistory } from 'react-router-dom'
+
+const SearchForm = () => {
+  const history = useHistory()
+  const formik = useFormik({
+        initialValues: {
+            searchName: '',
+            searchStartDate: '',
+            searchEndDate: ''       
+        },
+        onSubmit: values => {
+            const searchLink = `/search?q=${values.searchName}&startDate=${values.searchStartDate}&endDate=${values.searchEndDate}`
+            history.push({searchLink})
+        },
+    })
+    return (
+       <form onSubmit={formik.handleSubmit}>
+       <input
+         id="searchName"
+         name="searchName"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.searchName}
+       />
+       <input
+         id="searchStartDate"
+         name="searchStartDate"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.searchStartDate}
+       />
+       <input
+         id="searchEndDate"
+         name="searchEndDate"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.searchEndDate}
+       />
+       <button type="submit">Submit</button>
+     </form>
+    )
+
+}
+
+export default SearchForm
