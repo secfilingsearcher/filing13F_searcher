84a13655585668569c9a1a4b4f7f54bfaeb0a9ff
rosealti <71480280+rosealti@users.noreply.github.com>
2021-10-20 21:34:29 -0400

    Renamed fields in FlaskSqlAlchemyTestConfiguration in test_filters.py. (#160)
    * Renamed fields in FlaskSqlAlchemyTestConfiguration in test_filters.py.

--- python/tests/api/routes/test_filters.py
+++ python/tests/api/routes/test_filters.py
@@ -5,18 +5,18 @@ from datetime import datetime, date
 from flask import Flask
 from flask_testing import TestCase
 
-from edgar_filing_searcher.api.routes.filters import filter_company_by_date, filter_edgar_filing_by_date
+from edgar_filing_searcher.api.routes.filters import filter_company_by_date, \
+    filter_edgar_filing_by_date
 from edgar_filing_searcher.database import db
 from edgar_filing_searcher.models import EdgarFiling, Company, Data13f
-from edgar_filing_searcher.parsers.parser_utils import send_data_to_db
 
-COMPANY_CIK_1 = "0001171592"
-ACCESSION_NO_TABLE1_ROW1 = '0001420506-21-000830'
-ACCESSION_NO_TABLE1_ROW2 = '00016273506-21-000830'
-COMPANY_CIK_2 = "0006734892"
-ACCESSION_NO_2 = '000384934-14-0034330'
-COMPANY_CIK_3 = "0008322302"
-ACCESSION_NO_3 = '000238234-23-0238930'
+COMPANY_CIK_COOL = "0001171592"
+ACCESSION_NO_COOL = '0001420506-21-000830'
+OTHER_ACCESSION_NO_COOL = '00016273506-21-000830'
+COMPANY_CIK_NICE = "0006734892"
+ACCESSION_NO_NICE = '000384934-14-0034330'
+COMPANY_CIK_PURPLE = "0008322302"
+ACCESSION_NO_PURPLE = '000238234-23-0238930'
 FALSE_COMPANY_CIK = "1"
 
 
@@ -41,88 +41,109 @@ class FlaskSqlAlchemyTestConfiguration(TestCase):
     def setUp(self):
         """Sets up a test database"""
         db.create_all()
-        self.company1 = Company(cik_no=COMPANY_CIK_1, company_name="Cool Industries", filing_count=1)
-        self.edgar_filing1_row1 = EdgarFiling(accession_no=ACCESSION_NO_TABLE1_ROW1, cik_no=COMPANY_CIK_1,
-                                              filing_date=datetime.fromisoformat("1999-09-01"))
-        self.data_13f_table1_row1 = [Data13f(equity_holdings_id="67896567",
-                                             accession_no=ACCESSION_NO_TABLE1_ROW1,
-                                             cik_no='56464565767',
-                                             name_of_issuer='Agilent Technologies',
-                                             title_of_class='COM',
-                                             cusip='00846U101',
-                                             value=22967078.5,
-                                             ssh_prnamt=180644,
-                                             ssh_prnamt_type='None',
-                                             put_call='None',
-                                             investment_discretion='SOLE',
-                                             other_manager='None',
-                                             voting_authority_sole=22967078,
-                                             voting_authority_shared=0,
-                                             voting_authority_none=0
-                                             )]
-        self.edgar_filing1_row2 = EdgarFiling(accession_no=ACCESSION_NO_TABLE1_ROW2, cik_no=COMPANY_CIK_1,
-                                              filing_date=datetime.fromisoformat("1998-05-02"))
-        self.data_13f_table1_row2 = [Data13f(equity_holdings_id="673326567",
-                                             accession_no=ACCESSION_NO_TABLE1_ROW2,
-                                             cik_no='3349665767',
-                                             name_of_issuer='Flight Technologies',
-                                             title_of_class='COM',
-                                             cusip='0584GU101',
-                                             value=34967078.5,
-                                             ssh_prnamt=2670644,
-                                             ssh_prnamt_type='None',
-                                             put_call='None',
-                                             investment_discretion='SOLE',
-                                             other_manager='None',
-                                             voting_authority_sole=4257078,
-                                             voting_authority_shared=0,
-                                             voting_authority_none=0
-                                             )]
+        self.company_cool = Company(cik_no=COMPANY_CIK_COOL, company_name="Cool Industries",
+                                    filing_count=1)
+        self.edgar_filing_cool = EdgarFiling(accession_no=ACCESSION_NO_COOL,
+                                             cik_no=COMPANY_CIK_COOL,
+                                             filing_date=datetime.fromisoformat("1999-09-01"))
+        self.other_edgar_filing_cool = EdgarFiling(accession_no=OTHER_ACCESSION_NO_COOL,
+                                                   cik_no=COMPANY_CIK_COOL,
+                                                   filing_date=datetime.fromisoformat("1998-05-02"))
+        self.data_13f_cool = [Data13f(equity_holdings_id="67896567",
+                                      accession_no=ACCESSION_NO_COOL,
+                                      cik_no='56464565767',
+                                      name_of_issuer='Agilent Technologies',
+                                      title_of_class='COM',
+                                      cusip='00846U101',
+                                      value=22967078.5,
+                                      ssh_prnamt=180644,
+                                      ssh_prnamt_type='None',
+                                      put_call='None',
+                                      investment_discretion='SOLE',
+                                      other_manager='None',
+                                      voting_authority_sole=22967078,
+                                      voting_authority_shared=0,
+                                      voting_authority_none=0
+                                      )]
+        self.other_data_13f_cool = [Data13f(equity_holdings_id="673326567",
+                                            accession_no=OTHER_ACCESSION_NO_COOL,
+                                            cik_no='3349665767',
+                                            name_of_issuer='Flight Technologies',
+                                            title_of_class='COM',
+                                            cusip='0584GU101',
+                                            value=34967078.5,
+                                            ssh_prnamt=2670644,
+                                            ssh_prnamt_type='None',
+                                            put_call='None',
+                                            investment_discretion='SOLE',
+                                            other_manager='None',
+                                            voting_authority_sole=4257078,
+                                            voting_authority_shared=0,
+                                            voting_authority_none=0
+                                            )]
 
-        self.company2 = Company(cik_no=COMPANY_CIK_2, company_name="Nice Industries", filing_count=1)
-        self.edgar_filing2 = EdgarFiling(accession_no=ACCESSION_NO_2, cik_no=COMPANY_CIK_2,
-                                         filing_date=datetime.fromisoformat("2000-04-05"))
-        self.data_13f_table2 = [Data13f(equity_holdings_id="4364347",
-                                        accession_no=ACCESSION_NO_2,
-                                        cik_no='56443437',
-                                        name_of_issuer='Moon Labs',
-                                        title_of_class='COM',
-                                        cusip='008346101',
-                                        value=6599834078.5,
-                                        ssh_prnamt=1804594,
-                                        ssh_prnamt_type='None',
-                                        put_call='None',
-                                        investment_discretion='SOLE',
-                                        other_manager='None',
-                                        voting_authority_sole=54522278,
-                                        voting_authority_shared=0,
-                                        voting_authority_none=0
-                                        )]
+        self.company_nice = Company(cik_no=COMPANY_CIK_NICE, company_name="Nice Industries",
+                                    filing_count=1)
+        self.edgar_filing_nice = EdgarFiling(accession_no=ACCESSION_NO_NICE,
+                                             cik_no=COMPANY_CIK_NICE,
+                                             filing_date=datetime.fromisoformat("2000-04-05"))
+        self.data_13f_table_nice = [Data13f(equity_holdings_id="4364347",
+                                            accession_no=ACCESSION_NO_NICE,
+                                            cik_no='56443437',
+                                            name_of_issuer='Moon Labs',
+                                            title_of_class='COM',
+                                            cusip='008346101',
+                                            value=6599834078.5,
+                                            ssh_prnamt=1804594,
+                                            ssh_prnamt_type='None',
+                                            put_call='None',
+                                            investment_discretion='SOLE',
+                                            other_manager='None',
+                                            voting_authority_sole=54522278,
+                                            voting_authority_shared=0,
+                                            voting_authority_none=0
+                                            )]
 
-        self.company3 = Company(cik_no=COMPANY_CIK_3, company_name="Purple Industries", filing_count=1)
-        self.edgar_filing3 = EdgarFiling(accession_no=ACCESSION_NO_3, cik_no=COMPANY_CIK_3,
-                                         filing_date=datetime.fromisoformat("2001-07-05"))
-        self.data_13f_table3 = [Data13f(equity_holdings_id="39023347",
-                                        accession_no=ACCESSION_NO_3,
-                                        cik_no='823902437',
-                                        name_of_issuer='Star Companies',
-                                        title_of_class='COM',
-                                        cusip='00923801',
-                                        value=9023734078.5,
-                                        ssh_prnamt=389594,
-                                        ssh_prnamt_type='None',
-                                        put_call='None',
-                                        investment_discretion='SOLE',
-                                        other_manager='None',
-                                        voting_authority_sole=9823278,
-                                        voting_authority_shared=0,
-                                        voting_authority_none=0
-                                        )]
+        self.company_purple = Company(cik_no=COMPANY_CIK_PURPLE, company_name="Purple Industries",
+                                      filing_count=1)
+        self.edgar_filing_purple = EdgarFiling(accession_no=ACCESSION_NO_PURPLE,
+                                               cik_no=COMPANY_CIK_PURPLE,
+                                               filing_date=datetime.fromisoformat("2001-07-05"))
+        self.data_13f_table_purple = [Data13f(equity_holdings_id="39023347",
+                                              accession_no=ACCESSION_NO_PURPLE,
+                                              cik_no='823902437',
+                                              name_of_issuer='Star Companies',
+                                              title_of_class='COM',
+                                              cusip='00923801',
+                                              value=9023734078.5,
+                                              ssh_prnamt=389594,
+                                              ssh_prnamt_type='None',
+                                              put_call='None',
+                                              investment_discretion='SOLE',
+                                              other_manager='None',
+                                              voting_authority_sole=9823278,
+                                              voting_authority_shared=0,
+                                              voting_authority_none=0
+                                              )]
 
-        send_data_to_db(self.company1, self.edgar_filing1_row1, self.data_13f_table1_row1)
-        send_data_to_db(self.company1, self.edgar_filing1_row2, self.data_13f_table1_row2)
-        send_data_to_db(self.company2, self.edgar_filing2, self.data_13f_table2)
-        send_data_to_db(self.company3, self.edgar_filing3, self.data_13f_table3)
+        db.session.add(self.company_cool)
+        db.session.add(self.edgar_filing_cool)
+        db.session.add(self.other_edgar_filing_cool)
+        for data_13f_row in self.data_13f_cool:
+            db.session.add(data_13f_row)
+        for data_13f_row in self.other_data_13f_cool:
+            db.session.add(data_13f_row)
+
+        db.session.add(self.company_nice)
+        db.session.add(self.edgar_filing_nice)
+        for data_13f_row in self.data_13f_table_nice:
+            db.session.add(data_13f_row)
+
+        db.session.add(self.company_purple)
+        db.session.add(self.edgar_filing_purple)
+        for data_13f_row in self.data_13f_table_purple:
+            db.session.add(data_13f_row)
+        db.session.commit()
 
     def tearDown(self):
         """Tears down test database"""
