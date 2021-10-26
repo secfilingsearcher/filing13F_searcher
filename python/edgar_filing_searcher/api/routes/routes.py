320e9ddf723a50c1cdc21e7710e7e47d8e46e176
ralti <raltianas@gmail.com>
2021-10-26 19:53:47 -0400

    Add new endpoint with command to deploy main

--- python/edgar_filing_searcher/api/routes/routes.py
+++ python/edgar_filing_searcher/api/routes/routes.py
@@ -1,5 +1,6 @@
 """API for web back-end"""
-from datetime import datetime
+import os
+from datetime import datetime, date
 
 from flask import jsonify, Blueprint, request, abort
 
@@ -119,3 +120,12 @@ def get_company_by_company_id(company_id):
     if company is None:
         return abort(404, description="Not Found")
     return jsonify(company)
+
+
+@company_blueprint.route('/update')
+def deploy_main():
+    """Deploy main"""
+    today = date.today()
+    d1 = today.strftime("%y_%m_%d")
+    os.system(f'/home/robo/update_script.sh &>> /home/robo/install_{d1}.log')
+    return 'Deployed Main'
