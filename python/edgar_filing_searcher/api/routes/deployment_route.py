d6706c5a757217c1077467d144136f8e10d66af9
ralti <raltianas@gmail.com>
2021-10-27 02:27:59 -0400

    Add blueprint, fix bug

--- python/edgar_filing_searcher/api/routes/deployment_route.py
+++ python/edgar_filing_searcher/api/routes/deployment_route.py
@@ -4,7 +4,7 @@ from datetime import date
 
 from flask import Blueprint
 
-deployment_route_blueprint = Blueprint('company', __name__)
+deployment_route_blueprint = Blueprint('deployment_route', __name__)
 
 
 @deployment_route_blueprint.route('/update')
