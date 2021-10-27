8487eb5a85be26ccfdf0be7e62aebceb3df2234a
ralti <raltianas@gmail.com>
2021-10-27 03:53:56 -0400

    Add scripts, remove date from log

--- python/edgar_filing_searcher/api/routes/deployment.py
+++ python/edgar_filing_searcher/api/routes/deployment.py
@@ -1,6 +1,5 @@
 """Route for deployment"""
 import os
-from datetime import date
 
 from flask import Blueprint
 
@@ -10,7 +9,5 @@ deployment_blueprint = Blueprint('deployment', __name__)
 @deployment_blueprint.route('/update')
 def deploy_main():
     """Deploy main"""
-    today = date.today()
-    today_formatted = today.strftime("%y_%m_%d")
-    os.system(f'/home/robo/update_script.sh &>> /home/robo/install_{today_formatted}.log')
+    os.system('/home/robo/update_script.sh &>> /home/robo/deploy.log')
     return 'Deployed Main Branch'
