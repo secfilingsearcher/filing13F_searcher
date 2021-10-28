c463fa617f7292e968d76d61df4b6282886f9c89
eharley19 <48498743+eharley19@users.noreply.github.com>
2021-10-27 21:57:45 -0400

    Revised search form with improved validation (#175)
    * Additonal css for filings page
    
    * Remove old validation schema from search form
    
    * Import yup library and add validation schema
    
    * Change to validation schema - date input blocked due to mismatch format
    
    * Add tooltip to appear conditionally when searchName blank
    
    * Fix typo in index.html file
    
    * Remove date search option
    
    * Updated searchform component test
    
    * Remove uunnecessary line of code and fix variable syntax

--- frontend/13f-app/src/tests/SearchForm.test.js
+++ frontend/13f-app/src/tests/SearchForm.test.js
@@ -3,10 +3,8 @@ import renderer from 'react-test-renderer'
 import SearchForm from '../components/SearchForm'
 
 test('SearchForm renders', ()=> {
-    // October 20th, 2021
-  const date = (new Date(2021, 10, 20)).toISOString().substr(0, 10)
   const component = renderer.create(
-        <SearchForm startDate={date} endDate={date}/>
+        <SearchForm />
   )
 let tree = component.toJSON()
 expect(tree).toMatchSnapshot()
