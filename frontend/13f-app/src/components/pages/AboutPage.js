63f141cfcd0c4108f729836012133003fd906383
Ed <edharley19@gmail.com>
2021-11-01 22:24:17 -0400

    Add just p tag text

--- frontend/13f-app/src/components/pages/AboutPage.js
+++ frontend/13f-app/src/components/pages/AboutPage.js
@@ -8,7 +8,7 @@ function About () {
                 <br></br>
                 <div className='bg-dark text-white col-md-8' id='story-panel'>
                     <h3>About 13F Filing Searcher</h3>
-                    <p>This site was created through the collaboration of two aspiring software engineers, Rose Altianas(<a>www.github.com</a>) and Edward Harley(<a>www.github.com</a>), under the mentorship and supervision of two industry professionals.</p>
+                    <p>This site was created through the collaboration of two aspiring software engineers, Rose Altianas (<a>www.github.com</a>) and Edward Harley (<a>www.github.com</a>), under the mentorship and supervision of two industry professionals.</p>
                 </div>
                 <div id='image-panel'></div>
             </div>
