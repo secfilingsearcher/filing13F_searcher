95b6a99e39af59ac6669bd9b4e3ff573ef184b75
Kacy James <inquen@gmail.com>
2021-11-02 22:03:05 -0400

    Made some CSS edits, added jsconfig.json

--- frontend/13f-app/src/components/pages/AboutPage.js
+++ frontend/13f-app/src/components/pages/AboutPage.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import '../AboutPage.css'
+import bowlingGreenImage from 'images/img-1.jpg'
 
 function About () {
   return (
         <>
             <div className='container' id='about-main'>
-                <br></br>
+                <br />
                 <div className='bg-dark text-white col-md-8' id='story-panel'>
+                    <div id='bio-contents'>
                     <h3>About 13F Filing Searcher</h3>
-                    <p>This site was created through the collaboration of two aspiring software engineers, Rose Altianas (<a>www.github.com</a>) and Edward Harley (<a>www.github.com</a>), under the mentorship and supervision of two industry professionals.</p>
+                    <hr />
+                    <p>This site was created through the collaboration of two aspiring software engineers,  <a className="link-light" href="https://github.com/rosealti">Rose Altianas</a> and <a className="link-light" href="https://github.com/eharley19">Edward Harley</a>, under the mentorship and supervision of two industry professionals.</p>
+                    <p>Built with:</p>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item"><h4><span className="badge bg-secondary">Python</span></h4></li>
+                        <li className="list-group-item"><h4><span className="badge bg-secondary">Flask</span></h4></li>
+                        <li className="list-group-item"><h4><span className="badge bg-secondary">Flask-SQLAlchemy</span></h4></li>
+                        <li className="list-group-item"><h4><span className="badge bg-secondary">JavaScript</span></h4></li>
+                        <li className="list-group-item"><h4><span className="badge bg-secondary">React</span></h4></li>
+                        <li className="list-group-item"><h4><span className="badge bg-secondary">Bootstrap</span></h4></li>
+                    </ul>
+                    </div>
+                    <img id="bowling-green-image" src={bowlingGreenImage} alt="bowling green statue" />
                 </div>
-                <div id='image-panel'></div>
             </div>
         </>
   )
